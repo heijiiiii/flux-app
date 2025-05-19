e4279373c18318cacaa25a665967a24708f52c10
unknown <jrcookie0308@gmail.com>
2025-05-19 16:40:52 +0900

    fix: update use-toast and use-sidebar hooks

--- hooks/use-sidebar.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-export function useSidebar() {
-  const [open, setOpen] = useState(false);
-  const toggle = () => setOpen((v) => !v);
-  return { open, toggle };
-}
